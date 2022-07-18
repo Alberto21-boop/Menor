e603cebf4e30c103e4e9f42459f4558592d016ce
alber <alber@ALBERTO_BARBOSA>
2022-07-18 15:09:35 -0300

    Adicionar arquivos de projeto.

---
+++ Menor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Menor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Fazer um programa para ler três números inteiros. Em seguida, mostrar qual é o menor
+            //dentre os três números lidos. Em caso de empate, mostrar apenas uma vez.
+
+            int A, B, C;
+
+            Console.WriteLine("Boa tarde, digite por favor três numeros inteiros: ");
+            Console.Write("Primeiro Número: ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("Segundo Número: ");
+            B = int.Parse(Console.ReadLine());
+            Console.Write("Terceiro Número: ");
+            C = int.Parse(Console.ReadLine());
+
+            if((A < B) && (A < C))
+            {
+                Console.WriteLine("MENOR = " + A);
+            }
+            else if((B < C) && (B < A))
+            {
+                Console.WriteLine("MENOR = " + B);
+            }
+            else
+            {
+                Console.WriteLine("MENOR = " + C);
+            }
+
+        }
+    }
+}
